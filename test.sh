dba7c32f82a452d772631ddd5bda30736886d4ed
The Full-Stack Jonin <francis94c@gmail.com>
2026-02-02 06:03:26 +0100

    test: Parallel mode

--- test.sh
+++ test.sh
@@ -3,5 +3,6 @@
 export INPUT_SOURCE_FILE="./app_en.arb"
 export INPUT_TARGET_FILE="./app_es.arb, ./app_fr.arb, ./app_de.arb"
 export INPUT_TARGET_LANG_CODE="es, fr, de"
+export INPUT_PARALLEL="true"
 
 node index.js
